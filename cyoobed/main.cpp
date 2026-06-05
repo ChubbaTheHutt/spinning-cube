c7a102e4a673e0a93520d0d7b080b47bd0f458e5
ChubbaTheHutt <124078662+ChubbaTheHutt@users.noreply.github.com>
2026-06-05 01:09:25 -0400

    Boilerplate for menu bar

--- cyoobed/main.cpp
+++ cyoobed/main.cpp
@@ -4,19 +4,38 @@
 #include <QMenuBar>
 #include <QMenu>
 #include <QAction>
+#include <QKeySequence>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
 
-    QMenuBar qmb(&w);
+    //MENU BAR (Move to new file?)
+    QMenuBar *qmb = w.menuBar();
 
-    QMenu file_menu("file", &qmb);
-    QMenu edit_menu("edit", &qmb);
-    QMenu help_menu("help", &qmb);
+    //MENUS
+    QMenu *fileMenu = qmb->addMenu("&File");
+    QMenu *editMenu = qmb->addMenu("&Edit");
+    editMenu->setTearOffEnabled(true);
+    QMenu *helpMenu = qmb->addMenu("&Help");
 
-    qmb.show();
+    //submenus
+    QMenu *shapesMenu = editMenu->addMenu("shapes");
+
+    //ACTIONS
+    QAction *saveActn = fileMenu->addAction("save");
+    saveActn->setShortcut(Qt::CTRL | Qt::Key_P);
+    QAction *loadActn = fileMenu->addAction("load");
+    QAction *exitActn = fileMenu->addAction("exit");
+
+    QAction *squareShapeActn = shapesMenu->addAction("quad");
+    QAction *triShapeActn = shapesMenu->addAction("tri");
+    QAction *lineShapeActn = shapesMenu->addAction("line");
+
+    QAction *showShortcutsActn = helpMenu->addAction("show shortcuts");
+
+
     w.show();
     return QApplication::exec();
 }
